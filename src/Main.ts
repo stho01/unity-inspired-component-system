9dad96ebbf6f6f9e4745a4859f4378fc463caa5a
Sten Marius <stho@oxx.no>
2017-11-13 23:25:49 +0100

    Add a test behaviour class for testing the update logic.

--- src/Main.ts
+++ src/Main.ts
@@ -4,19 +4,23 @@ import { Player } from "./game/gameobjects/player";
 import { ShapeRenderer } from "./engine/components/ShapeRenderer";
 import { Circle } from "./engine/geometry/Circle";
 import { Vector2D } from "./engine/math/Vector2D";
+import { CenterBehaviour } from "./game/components/CenterBehaviour";
 
 let game = new Game({clearColor: "wheat"});
 
 
 let initialScene: Scene = new Scene(game);
 let player: Player = new Player(initialScene);
+player.transform.position = new Vector2D(game.viewPort.width/2, game.viewPort.height/2);
 
 let renderer = new ShapeRenderer(player);
 renderer.color = "blue";
 renderer.shape = new Circle(30);
 
 player.attachComponent(ShapeRenderer, renderer);
-player.transform.position = new Vector2D(game.viewPort.width/2, game.viewPort.height/2);
+player.attachComponent(CenterBehaviour, new CenterBehaviour(player));
+
+
 
 initialScene.addGameObject(player);
 
---
+++ src/game/components/CenterBehaviour.ts
@@ -0,0 +1,17 @@
+import { Behaviour } from "../../engine/components/Behaviour";
+import { Transform } from "../../engine/components/Transform";
+import { Vector2D } from "../../engine/math/Vector2D";
+
+export class CenterBehaviour extends Behaviour {
+
+    private _transform: Transform;
+
+    initialize() {
+        this._transform = this._owner.getComponent(Transform) as Transform;
+    }
+
+    update(deltaTime: number): void {
+        let viewPort = this._owner.scene.game.viewPort;
+        this._transform.position = new Vector2D(viewPort.width/2, viewPort.height/2);
+    }
+}
