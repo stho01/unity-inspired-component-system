1cbb47446312a08d85c574f2c2ea642897bb3157
Sten Marius <sten.marius.holmen@gmail.com>
2022-04-04 22:59:32 +0200

    Wip ECS

--- example3/src/Main.ts
+++ example3/src/Main.ts
@@ -19,10 +19,17 @@ player.addComponent(Sprite, "/box.png", 10, 20);
 
 game.sceneManager.push(scene);
 
+
+/*
+let scene2: Scene = new Scene(game);
+const player2 = scene2.createEntity();
+player2.addComponent(Transform, Vector2D.from(100, 100));
+player2.addComponent(RigidBody, Vector2D.from(100, 0));
+player2.addComponent(Sprite, "/box.png", 10, 20);
+setTimeout(() => {
+    game.sceneManager.push(scene2);
+}, 6000);
+*/
+
 game.init();
 game.run();
-
-// initialize game
-//game.init();
-// run game loop
-//game.run();
